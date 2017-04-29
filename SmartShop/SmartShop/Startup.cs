87bcf604385feaa7e00c58343f3a1077a80b91d2
Minh Thuan Tran <iamthuan@hotmail.com>
2017-04-29 18:16:59 +0930

    Using ASP.NET MVC template.

---
+++ SmartShop/SmartShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartShop.Startup))]
+namespace SmartShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
