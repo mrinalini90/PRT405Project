0b2883837492a2ad78a19bfe8b9e771d066daf8d
Minh Thuan Tran <iamthuan@hotmail.com>
2017-05-18 13:38:51 +0930

    Turn on Lockout.

--- SmartShop/SmartShop/Controllers/AccountController.cs
+++ SmartShop/SmartShop/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace SmartShop.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: true);  // turn on lockout
             switch (result)
             {
                 case SignInStatus.Success:
