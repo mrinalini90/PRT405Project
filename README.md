49ee392b8558da340bc9bbecb43bc30c0a6810d1
Meeru <mirupikachu@gmail.com>
2017-05-26 03:13:59 +0930

    Basic Product CRUD and Read me file updated with setup instructions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PRT405Project
 Group Project Repository 
+
+Installation
+
+1. Download the repository and unzip the files. The latest working code can be found in the Smart Shop folder, the old work and work done by whole team can be found in the old folder.
+
+2. If on campus server is accessable the following connection string should be used
+	// Connection string to the server fb
+
+3. To setup a local repository
+	a. Create a database called "SmartShop" on your sql express server, through SQL server management studio.
+	b. add the following connection strings in the web.config file 
+		  <add name="DefaultConnection" connectionString=" Server=localhost\SQLEXPRESS; Database=SmartShop; Trusted_Connection=True;Integrated Security=True;" providerName="System.Data.SqlClient" />
+		  <add name="SmartShopEntities" connectionString="metadata=res://*/Models.SmartShop.csdl|res://*/Models.SmartShop.ssdl|res://*/Models.SmartShop.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=.\sqlexpress;initial catalog=SmartShop;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;" providerName="System.Data.EntityClient" /></connectionStrings>
+	c. run the db-setup.sql code on your SmartShop database.
+	
---
+++ SmartShop/SmartShop/Controllers/ProductsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using SmartShop.Models;
+
+namespace SmartShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private SmartShopEntities db = new SmartShopEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var currentUser = User.Identity.GetUserId();
+            var products = db.Products.Include(p => p.AspNetUser).Where(p => p.AspNetUser.Id == currentUser);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductName,ProductCategory,ProductDescription,ProductPrice,UserID")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", product.UserID);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", product.UserID);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,ProductCategory,ProductDescription,ProductPrice,UserID")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", product.UserID);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
