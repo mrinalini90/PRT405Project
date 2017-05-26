153b7e30bde2b224ea680d02f2e6fc2476e92ce8
jithin jose <jitzjose@yahoo.com>
2017-05-26 20:40:17 +0930

    Changes with Shopping Cart Features
    Updated

---
+++ WebApplication1/WebApplication1/Controllers/Cart_UserController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+using Microsoft.AspNet.Identity;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    public class Cart_UserController : Controller
+    {
+        private PRT405_2017SEM1Entities db = new PRT405_2017SEM1Entities();
+        AccountController a = new AccountController();
+
+        // GET: Cart_User
+        public ActionResult Index()
+        {
+            string UserName = User.Identity.GetUserName();
+            if (UserName != "")
+            {
+                return View(db.Carts.Where(c => c.UserName == UserName).ToList());
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        
+        
+        // GET: Cart_User/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            Cart Cart_User = db.Carts.Find(id);
+            db.Carts.Remove(Cart_User);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // POST: Cart_User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Product Cart_User = db.Products.Find(id);
+            db.Products.Remove(Cart_User);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Cart_User");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/PeopleController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class PeopleController : Controller
+    {
+        private PRT405_2017SEM1Entities db = new PRT405_2017SEM1Entities();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            if(userId == "admin@admin.com")
+            {
+                return View(db.Products.ToList());
+            }
+            var returnList = db.Products.Where(x => x.UserName == userId).ToList();
+            return View(returnList);
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var userDetails = this.HttpContext.User.Identity.Name;
+            var userId = User.Identity.GetUserId();
+
+
+            Product s279330_User = db.Products.Find(id);
+            if (s279330_User == null)
+            {
+                return HttpNotFound();
+            }
+            return View(s279330_User);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,UserId,CarName,Details,Address")] Product s279330_User)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDetails = this.HttpContext.User.Identity.Name;
+                var userId = User.Identity.GetUserId();
+
+                s279330_User.ProductName = userId;
+
+                db.Products.Add(s279330_User);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(s279330_User);
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product s279330_User = db.Products.Find(id);
+            if (s279330_User == null)
+            {
+                return HttpNotFound();
+            }
+            return View(s279330_User);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,UserId,CarName,Details,Address")] Product s279330_User)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(s279330_User).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(s279330_User);
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product s279330_User = db.Products.Find(id);
+            if (s279330_User == null)
+            {
+                return HttpNotFound();
+            }
+            return View(s279330_User);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Product s279330_User = db.Products.Find(id);
+            db.Products.Remove(s279330_User);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
